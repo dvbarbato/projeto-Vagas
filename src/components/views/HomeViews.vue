a2e65facf74b816f1b81575f8e4bb478a5e22bd8
David Barbato <daviddev1306@gmail.com>
2022-05-06 21:01:43 -0300

    implemented PesquisarVaga component

--- src/components/views/HomeViews.vue
+++ src/components/views/HomeViews.vue
@@ -2,23 +2,7 @@
   <div class="container py-4">
     <div class="row">
       <div class="col">
-        <div class="h-100 p-5 bg-light border rounded-3">
-          <h2>Pesquisar vagas</h2>
-          <div class="row">
-            <div class="col">
-              <div class="form-group">
-                <label>Título da Vaga</label>
-                <input type="text" class="form-control" placeholder="Pesquise por palavras chaves">
-                <small class="form-text text-muted">Informe palavras que estejam relacionadas com o título da vaga que você procura.</small>
-              </div>
-            </div>
-          </div>
-          <div class="row">
-            <div class="col">
-              <button class="btn btn-outline-dark mt-2" type="button">Buscar</button>
-            </div>
-          </div>
-        </div>
+        <pesquisar-vaga></pesquisar-vaga>
       </div>
     </div>
     <div class="row mt-5">
@@ -48,7 +32,11 @@
 </template>
 
 <script>
+import PesquisarVaga from '@/components/comuns/PesquisarVaga.vue'
 export default {
+  components: {
+    PesquisarVaga
+  }
   
 }
 </script>
